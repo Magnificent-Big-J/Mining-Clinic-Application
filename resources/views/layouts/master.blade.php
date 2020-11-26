76a92cfdaa99add00ca5ea75e2ad1dffd3de27d7
Joel Mnisi <joel@mdsholdings.co.za>
2020-11-26 20:46:33 +0200

    Integrating admin template

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,76 @@
+
+<!DOCTYPE html>
+<html lang="en">
+
+<!-- Mirrored from dreamguys.co.in/demo/doccure/admin/appointment-list.html by HTTrack Website Copier/3.x [XR&CO'2014], Sat, 30 Nov 2019 04:12:46 GMT -->
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0">
+    <title>Doccure - Appointment List Page</title>
+
+    <!-- Favicon -->
+    <link rel="shortcut icon" type="image/x-icon" href="assets/img/favicon.png">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="{{asset('admin/assets/css/bootstrap.min.css')}}">
+
+    <!-- Fontawesome CSS -->
+    <link rel="stylesheet" href="{{asset('admin/assets/css/font-awesome.min.css')}}">
+
+    <!-- Feathericon CSS -->
+    <link rel="stylesheet" href="{{asset('admin/assets/css/feathericon.min.css')}}">
+
+    <!-- Datatables CSS -->
+    <link rel="stylesheet" href="{{asset('admin/assets/plugins/datatables/datatables.min.css')}}">
+
+    <!-- Main CSS -->
+    <link rel="stylesheet" href="{{asset('admin/assets/css/style.css')}}">
+
+    <!--[if lt IE 9]>
+    <script src="{{asset('admin/assets/js/html5shiv.min.js')}}"></script>
+    <script src="{{asset('admin/assets/js/respond.min.js')}}"></script>
+    <![endif]-->
+</head>
+<body>
+
+<!-- Main Wrapper -->
+<div class="main-wrapper" id="app">
+
+    <!-- Header -->
+    @include('partials.navbar')
+    <!-- /Header -->
+
+    <!-- Sidebar -->
+    @include('partials.sidebar')
+    <!-- /Sidebar -->
+
+    <!-- Page Wrapper -->
+    <div class="page-wrapper">
+       @yield('content')
+    </div>
+    <!-- /Page Wrapper -->
+
+</div>
+<!-- /Main Wrapper -->
+
+<!-- jQuery -->
+<script src="{{asset('admin/assets/js/jquery-3.2.1.min.js')}}"></script>
+<script src="{{asset('js/app.js')}}"></script>
+<!-- Bootstrap Core JS -->
+<script src="{{asset('admin/assets/js/popper.min.js')}}"></script>
+<script src="{{asset('admin/assets/js/bootstrap.min.js')}}"></script>
+
+<!-- Slimscroll JS -->
+<script src="{{asset('admin/assets/plugins/slimscroll/jquery.slimscroll.min.js')}}"></script>
+
+<!-- Datatables JS -->
+<script src="{{asset('admin/assets/plugins/datatables/jquery.dataTables.min.js')}}"></script>
+<script src="{{asset('admin/assets/plugins/datatables/datatables.min.js')}}"></script>
+
+<!-- Custom JS -->
+<script  src="{{asset('admin/assets/js/script.js')}}"></script>
+
+</body>
+
+<!-- Mirrored from dreamguys.co.in/demo/doccure/admin/appointment-list.html by HTTrack Website Copier/3.x [XR&CO'2014], Sat, 30 Nov 2019 04:12:49 GMT -->
+</html>
