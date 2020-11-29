4e1d45d57ce6a21a7dc2476abc5f2e488955cbad
Joel Mnisi <joel@mdsholdings.co.za>
2020-11-29 21:29:45 +0200

    Seeding Super user

--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\User;
 use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
 
 class UserSeeder extends Seeder
 {
@@ -11,6 +13,15 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $user = User::create([
+            'first_name' => 'Invoke',
+            'last_name' => 'Solutions',
+            'title' => 'Mr',
+            'email' => 'info@invokesolutions.co.za',
+            'password' => bcrypt('p@ssword'),
+        ]);
+
+        $role = Role::findById(1);
+        $user->assignRole([$role->id]);
     }
 }
