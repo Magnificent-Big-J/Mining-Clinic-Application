41c0d2db2ff43d1a4efca903c12b717385709554
Joel Mnisi <joel@mdsholdings.co.za>
2020-11-30 08:17:21 +0200

    Disable registration and make login page landing page

--- routes/web.php
+++ routes/web.php
@@ -13,12 +13,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
+Route::redirect('/', '/home');
 Auth::routes();
-
+Auth::routes(['register' => false]);
 Route::get('/home', 'HomeController@index')->name('home');
 Route::get('hello', function (){
     return view('admin.patients.index');
