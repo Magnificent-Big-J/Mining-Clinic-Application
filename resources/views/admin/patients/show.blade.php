38e119914b19c89e664d6a5553c58f77cfdeed15
Joel Mnisi <joel@mdsholdings.co.za>
2020-12-01 09:16:21 +0200

    Display address informatiom

--- resources/views/admin/patients/show.blade.php
+++ resources/views/admin/patients/show.blade.php
@@ -55,7 +55,42 @@
                     </div>
                 </div>
                 <!-- /Recent Orders -->
+                <div class="card">
+                    <div class="card-body">
+                        <div class="card ">
+                            <div class="card-header">
+                                <h4 class="card-title">Patient AddressesInformation</h4>
+                            </div>
+                            <div class="card-body">
+                                <div class="row">
+                                    <div class="col-lg-6">
+                                        <h4 class="card-title">Physical Address</h4>
+                                        @if ($patient->addresses[0]->address_type_id == 1)
+                                            <p>{{$patient->addresses[0]->address_1}}</p>
+                                            <p>{{$patient->addresses[0]->address_2}}</p>
+                                            <p>{{$patient->addresses[0]->postal_code}}</p>
+                                            <p>{{$patient->addresses[0]->province->province_name}}</p>
+                                            @else
+                                            No Address record
+                                        @endif
+                                    </div>
+                                    <div class="col-lg-6">
+                                        <h4 class="card-title">Postal Address</h4>
+                                        @if ($patient->addresses->count() == 2)
 
+                                            <p>{{$patient->addresses[1]->address_1}}</p>
+                                            <p>{{$patient->addresses[1]->address_2}}</p>
+                                            <p>{{$patient->addresses[1]->postal_code}}</p>
+                                            <p>{{$patient->addresses[1]->province->province_name}}</p>
+                                        @else
+                                            No Address record
+                                        @endif
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
