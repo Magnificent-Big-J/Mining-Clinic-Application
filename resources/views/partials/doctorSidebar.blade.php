00018086a53ed00772008f2f3a87943a09f911c2
Aubrey Hlungwane <Hlungwaneak@invokesolutions.co.za>
2020-12-02 05:44:19 +0200

    update doctor side bar

--- resources/views/partials/doctorSidebar.blade.php
+++ resources/views/partials/doctorSidebar.blade.php
@@ -17,64 +17,27 @@
         <nav class="dashboard-menu">
             <ul>
                 <li>
-                    <a href="doctor-dashboard.html">
+                    <a href="/">
                         <i class="fas fa-columns"></i>
                         <span>Dashboard</span>
                     </a>
                 </li>
                 <li class="active">
-                    <a href="appointments.html">
+                    <a href="/">
                         <i class="fas fa-calendar-check"></i>
                         <span>Appointments</span>
                     </a>
                 </li>
                 <li>
-                    <a href="my-patients.html">
+                    <a href="/">
                         <i class="fas fa-user-injured"></i>
                         <span>My Patients</span>
                     </a>
                 </li>
                 <li>
-                    <a href="schedule-timings.html">
-                        <i class="fas fa-hourglass-start"></i>
-                        <span>Schedule Timings</span>
-                    </a>
-                </li>
-                <li>
-                    <a href="invoices.html">
-                        <i class="fas fa-file-invoice"></i>
-                        <span>Invoices</span>
-                    </a>
-                </li>
-                <li>
-                    <a href="reviews.html">
-                        <i class="fas fa-star"></i>
-                        <span>Reviews</span>
-                    </a>
-                </li>
-                <li>
-                    <a href="chat-doctor.html">
-                        <i class="fas fa-comments"></i>
-                        <span>Message</span>
-                        <small class="unread-msg">23</small>
-                    </a>
-                </li>
-                <li>
-                    <a href="doctor-profile-settings.html">
+                    <a href="/">
                         <i class="fas fa-user-cog"></i>
                         <span>Profile Settings</span>
-                    </a>
-                </li>
-                <li>
-                    <a href="social-media.html">
-                        <i class="fas fa-share-alt"></i>
-                        <span>Social Media</span>
-                    </a>
-                </li>
-                <li>
-                    <a href="doctor-change-password.html">
-                        <i class="fas fa-lock"></i>
-                        <span>Change Password</span>
                     </a>
                 </li>
                 <li>
