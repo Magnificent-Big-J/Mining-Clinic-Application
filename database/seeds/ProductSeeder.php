9facf711b86d65ef143ec0a30d3029768469f2c1
Joel Mnisi <joel@mdsholdings.co.za>
2020-12-14 17:07:20 +0200

    Define folder structure

--- database/seeds/ProductSeeder.php
+++ database/seeds/ProductSeeder.php
@@ -15,7 +15,7 @@ class ProductSeeder extends Seeder
         $products = array(
             array('product_code'=>'G01001','product_name'=>'Halothane (with vaporizer)','product_description'=>'Fluothane (halothane) is an inhalation anesthetic indicated for the induction and maintenance of general anesthesia','product_size'=>'250ml', 'product_unit'=>'Bottle','product_category_id'=> 1),
             array('product_code'=>'G01002','product_name'=>'Isoflurance Inhalation','product_description'=>'Isoflurane is a halogenated hydrocarbon that is commonly used as an animal anesthetic.','product_size'=>'100ml', 'product_unit'=>'Bottle','product_category_id'=> 1),
-            array('product_code'=>'G01003','product_name'=>'Ketamine Injection IP 10 mg/ml','product_description'=>'Ketamine Injection IP 10 mg/ml','product_description','product_size'=>'100ml', 'product_unit'=>'Vial','product_category_id'=> 1),
+            array('product_code'=>'G01003','product_name'=>'Ketamine Injection IP 10 mg/ml','product_description'=>'Ketamine Injection IP 10 mg/ml','product_size'=>'100ml', 'product_unit'=>'Vial','product_category_id'=> 1),
             array('product_code'=>'G01004','product_name'=>'etamine Injection IP 10 mg/ml','product_description'=>'etamine Injection IP 10 mg/ml','product_size'=>'2ml', 'product_unit'=>'Vial','product_category_id'=> 1),
             array('product_code'=>'G01005','product_name'=>'Nitrous Oxide Inhalation','product_description'=>'Nitrous Oxide Inhalation','product_size'=>'100 ml', 'product_unit'=>'Bottle','product_category_id'=> 1),
             array('product_code'=>'G01006','product_name'=>'Oxygen Inhalation','product_description'=>'Oxygen Inhalation','product_size'=>'', 'product_unit'=>'Bottle','product_category_id'=> 1),
