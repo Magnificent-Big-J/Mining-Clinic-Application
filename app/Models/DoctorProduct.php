e095dd2537b488a4ae36e2b8165bf63546b6f452
Joel Mnisi <joel@mdsholdings.co.za>
2020-12-14 15:41:34 +0200

    Model Product > 1. Setting the properties > 2. Adding fillable properties > 3. Defining relationships

--- app/Models/DoctorProduct.php
+++ app/Models/DoctorProduct.php
@@ -2,7 +2,6 @@
 
 namespace App\Models;
 
-use App\Product;
 use Illuminate\Database\Eloquent\Model;
 
 /**
--- app/Models/Product.php
+++ app/Models/Product.php
@@ -1,10 +1,33 @@
 <?php
 
-namespace App;
+namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Collection;
 
+/**
+ * Class Product
+ * @package App\Models
+ * @property int $id
+ * @property string $product_code
+ * @property string $product_name
+ * @property string $product_description
+ * @property string $product_size
+ * @property string $product_unit
+ * @property int $product_category_id
+ * @property ProductCategory $productCategory
+ * @property DoctorProduct[]|Collection $doctorProducts
+ */
 class Product extends Model
 {
-    //
+    protected $fillable = ['product_code', 'product_name', 'product_description', 'product_size', 'product_unit', 'product_category_id'];
+
+    public function productCategory()
+    {
+        return $this->belongsTo(Product::class);
+    }
+    public function doctorProducts()
+    {
+        return $this->hasMany(DoctorProduct::class);
+    }
 }
