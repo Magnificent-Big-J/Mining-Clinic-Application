3c9623df3ae34edf278b2839f12add70c9a15257
Joel Mnisi <joel@mdsholdings.co.za>
2020-12-14 15:44:24 +0200

    Model ProductCategory > 1. Setting the properties > 2. Adding fillable properties > 3. Defining relationships

--- app/Models/ProductCategory.php
+++ app/Models/ProductCategory.php
@@ -1,10 +1,23 @@
 <?php
 
-namespace App;
+namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Collection;
 
+/**
+ * Class ProductCategory
+ * @package App
+ * @property int $id
+ * @property string $name
+ * @property Product[]|Collection $products
+ */
 class ProductCategory extends Model
 {
-    //
+    protected $fillable = ['name'];
+
+    public function products()
+    {
+        return $this->hasMany(Product::class);
+    }
 }
