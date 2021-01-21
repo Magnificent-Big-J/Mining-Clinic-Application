33029befc88ebf2c7822eb168a8fbba77d1fdb38
Joel Mnisi <joel@mdsholdings.co.za>
2021-01-21 16:18:18 +0200

    spacing

--- resources/views/doctor/dashboard/partials/actions.blade.php
+++ resources/views/doctor/dashboard/partials/actions.blade.php
@@ -1,11 +1,11 @@
 <div class="row">
-    <div class="col-sm-2">
+    <div class="col-sm-2 mr-2 mb-2">
         <a href="{{route('doctor.appointment.details', $appointment->id)}}" class="btn btn-sm bg-info-light">
             <i class="far fa-eye"></i> View
         </a>
     </div>
     @if($appointment->status === \App\Models\Appointment::PENDING_STATUS)
-        <div class="col-sm-2">
+        <div class="col-sm-2 mr-2 mb-2">
             <form action="{{route('doctor.appointment.update', $appointment->id)}}" method="post">
                 <input type="hidden" name="status" value="{{\App\Models\Appointment::ACCEPTED_STATUS}}">
                 @csrf
@@ -15,7 +15,7 @@
                 </button>
             </form>
         </div>
-        <div class="col-sm-2">
+        <div class="col-sm-2 mr-2 mb-2">
             <form action="{{route('doctor.appointment.update', $appointment->id)}}" method="post">
                 <input type="hidden" name="status" value="{{\App\Models\Appointment::DECLINED_STATUS}}">
                 @csrf
@@ -29,7 +29,7 @@
     @elseif ($appointment->status === \App\Models\Appointment::ACCEPTED_STATUS)
         @if ($appointment->prescriptions->count())
 
-            <div class="col-sm-2">
+            <div class="col-sm-2 mr-2 mb-2">
                 <form action="{{route('doctor.appointment.update', $appointment->id)}}" method="post">
                     <input type="hidden" name="status" value="{{\App\Models\Appointment::DONE_STATUS}}">
                     @csrf
@@ -40,7 +40,7 @@
                 </form>
             </div>
         @else
-            <div class="col-sm-2">
+            <div class="col-sm-2 mr-2 mb-2">
                 <a href="{{route('doctor.prescriptions.appointment.index', $appointment->id)}}" class="btn btn-sm bg-primary-light">
                     <i class="far fa-asterisk"></i> Capture Prescriptions
                 </a>
