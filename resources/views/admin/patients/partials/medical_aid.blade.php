152ac8eba71a174136b7216d675cbf7375c5c82b
Joel Mnisi <joel@mdsholdings.co.za>
2021-02-03 08:39:14 +0200

    Alignment

--- resources/views/admin/patients/partials/medical_aid.blade.php
+++ resources/views/admin/patients/partials/medical_aid.blade.php
@@ -1,5 +1,5 @@
 
-{{$patient->has_medical}}
 @if ($patient->has_medical !== 'N/A')
-    <a href="{{route('admin.medicalAid.edit', $patient->medicalAid[0]->id)}}" class="btn btn-sm btn-primary">Edit</a>
+    <a href="{{route('admin.medicalAid.edit', $patient->medicalAid[0]->id)}}" class="btn btn-sm btn-primary mr-2">Edit</a>
 @endif
+{{$patient->has_medical}}
